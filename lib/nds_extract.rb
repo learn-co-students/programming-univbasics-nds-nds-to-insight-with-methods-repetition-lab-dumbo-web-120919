e31026b32b09a116df0a1e4f538a53bff72b2309
Jessica Natovich <jessicanatovich@yahoo.com>
2019-11-27 16:30:29 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,36 +1,19 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require_relative './directors_database'
-
-def directors_totals(source)
-  result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
-end
-
-def gross_for_director(d)
-  total = 0
-  index = 0
-
-  while index < d[:movies].length do
-    total += d[:movies][index][:worldwide_gross]
-    index += 1
-  end
-
-  total
-end
-
 def list_of_directors(source)
   # Write this implementation
+  source.map{ |d| d[:name] }
+  names = []
+  i = 0
+
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
+  end
+
+  names
 end
 
 def total_gross(source)
   # Write this implementation
-  #
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
   # 2. list_of_directors: names provides an Array of directors names (use
@@ -38,6 +21,18 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  list_of_directors(source).reduce(0){|total, name| total +=  directors_totals(source)[name]}
+  dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  i = 0
+
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
+  end
+
+  total
 end
-
-
--- spec/nds_extract_spec.rb
+++ spec/nds_extract_spec.rb
@@ -1,80 +1,38 @@
-require 'spec_helper'
+def list_of_directors(source)
+  # Write this implementation
+  source.map{ |d| d[:name] }
+  names = []
+  i = 0
 
-describe 'total_gross' do
-  it 'correctly totals the total gross' do
-    expect(total_gross(directors_database)).to eq(10355501925)
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
   end
+
+  names
 end
 
-describe 'list of directors' do
-  it 'correctly extracts :name keys out of an AoH where'  do
-    stooges = [{:name => "Larry"}, {:name => "Curly"}, {:name => "Moe"}, {:name => "Iggy"}]
-    expect(list_of_directors(stooges)).to eq(["Larry", "Curly", "Moe", "Iggy"])
+def total_gross(source)
+  # Write this implementation
+  # Should use methods:
+  # 1. directors_totals: returns a Hash of { dir_name => gross }
+  # 2. list_of_directors: names provides an Array of directors names (use
+  #
+  # Visit each key (i.e. director name), look up the value in the hash
+  # returned by directors_totals, and add it to a running total. When done,
+  # return the total
+  list_of_directors(source).reduce(0){|total, name| total +=  directors_totals(source)[name]}
+  dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  i = 0
+
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
   end
-end
 
-describe 'gross_for_director method' do
-  it "correctly totals the worldwide earnings for a director" do
-    first_director_name = directors_database.first.values.first
-    first_director_hash = directors_database.find{ |x| x[:name] == first_director_name }
-    expect(gross_for_director(first_director_hash)).to eq(1357566430)
-  end
-end
-
-describe 'The directors_database method can be processed by the directors_totals method' do
-  it 'which returns a Hash describing director to total' do
-    expect(directors_totals({})).to be_a(Hash)
-  end
-
-  describe "and correctly totals the directors' totals" do
-    let(:expected) {
-      {
-        "Stephen Spielberg"=>1357566430,
-        "Russo Brothers"=>2281002470,
-        "James Cameron"=>2571020373,
-        "Spike Lee"=>256624217,
-        "Wachowski Siblings"=>806180282,
-        "Robert Zemeckis"=>1273838385,
-        "Quentin Tarantino"=>662738268,
-        "Martin Scorsese"=>636812242,
-        "Francis Ford Coppola"=>509719258
-      }
-    }
-
-    it "correctly totals 'Stephen Spielberg'" do
-      expect(directors_totals(directors_database)['Stephen Spielberg']).to eq(expected['Stephen Spielberg'])
-    end
-
-    it "correctly totals 'Russo Brothers'" do
-      expect(directors_totals(directors_database)['Russo Brothers']).to eq(expected['Russo Brothers'])
-    end
-
-    it "correctly totals 'James Cameron'" do
-      expect(directors_totals(directors_database)['James Cameron']).to eq(expected['James Cameron'])
-    end
-
-    it "correctly totals 'Spike Lee'" do
-      expect(directors_totals(directors_database)['Spike Lee']).to eq(expected['Spike Lee'])
-    end
-
-    it "correctly totals 'Wachowski Siblings'" do
-      expect(directors_totals(directors_database)['Wachowski Siblings']).to eq(expected['Wachowski Siblings'])
-    end
-
-    it "correctly totals 'Robert Zemeckis'" do
-      expect(directors_totals(directors_database)['Robert Zemeckis']).to eq(expected['Robert Zemeckis'])
-    end
-
-    it "correctly totals 'Quentin Tarantino'" do
-      expect(directors_totals(directors_database)['Quentin Tarantino']).to eq(expected['Quentin Tarantino'])
-    end
-
-    it "correctly totals 'Martin Scorsese'" do
-      expect(directors_totals(directors_database)['Martin Scorsese']).to eq(expected['Martin Scorsese'])
-    end
-
-    it "correctly totals 'Francis Ford Coppola'" do
-      expect(directors_totals(directors_database)['Francis Ford Coppola']).to eq(expected['Francis Ford Coppola'])
-    end
-  end
+  total
 end
