d71d68193519bfb5a5f572c80e9dd77e255b30d6
Joel  Bly <jtb6179@g.rit.edu>
2019-12-04 19:16:05 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,15 +1,18 @@
+require "pry"
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
 def directors_totals(source)
   result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
+  director_index = 0 
+while director_index < source.length do 
+  directors_name = source[director_index]
+  result[directors_name[:name]] = gross_for_director(directors_name)
+  
+  director_index += 1 
+end
+return result 
 end
 
 def gross_for_director(d)
@@ -26,7 +29,18 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+x = 0 
+box = []
+while x < source.length do 
+  list_of_Ds = source[x][:name]
+  box << list_of_Ds
+ #
+  x += 1 
 end
+
+return box 
+
+end 
 
 def total_gross(source)
   # Write this implementation
@@ -38,6 +52,27 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  #binding.pry
+  di_index = 0
+  javalin = 0 
+  total = 0 
+  names = list_of_directors(source)
+  bunch = directors_totals(source)
+    
+     
+     while javalin < bunch.length do 
+       dir_name = names[di_index]
+       total += bunch[dir_name]
+       
+      # binding.pry
+      di_index += 1  
+     javalin += 1 
+   end
+     
+   
+     return total
+     
 end
 
 
